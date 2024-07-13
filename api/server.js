6ecd10915fbe767b0ff11da65adf11d6d6549ace
Sonoely Rivera <srivera@student.fullsail.edu>
2024-07-13 01:15:05 -0400

    commit route changes

--- api/server.js
+++ api/server.js
@@ -19,7 +19,13 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log("Database Connection Established"))
 
 app.use(express.json())
-app.use('/movies', movieRouter)
+app.use('/api/v1/movies', movieRouter)
+
+app.use(express.static(path.join(__dirname, '../reactjs/build')));
+
+app.get('/*', (req,res) => {
+    res.sendFile(path.join(__dirnmae, '../reactjs/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
